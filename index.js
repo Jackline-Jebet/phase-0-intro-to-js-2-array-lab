820e60f9f51c99fd3d86ddfc7142adfaa352cbd7
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-09 15:03:38 +0300

    commit tree c61e51b08eac654086100acd130560d37364252e

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
-var cats = ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"]
 
 function  destructivelyAppendCat(name){
     cats.push(name);
-    return cats;
 }
 
 function destructivelyPrependCat(name) {
 
     cats.unshift(name)
 
-    return cats;
 }
 
 function destructivelyRemoveLastCat(name) {
 
     cats.pop();
-    return cats;
+ 
 }
 
 function destructivelyRemoveFirstCat(name) {
     cats.shift();
-    return cats;
+    
 }
 
 function appendCat(name) {
 
-    let newcatArray = [...cats, name];
+    const newcatArray = [...cats, name];
     return newcatArray;
-}
-
-function removeLastCat() {
-    let newArrays=cats.slice(0,cats.length-1)
-
-    return newArrays;
-}
-
-function removeFirstCat() {
-
-let firstCat= cats.slice(1)
-    return firstCat;
-
 }
 
 function prependCat(name) {
 
-    let caats = [name, ...cats];
-    return caats;
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const newcatArray = cats.slice(0, -1);
+
+    return newcatArray;
+}
+
+function removeFirstCat() {
+
+let newCatArray = cats.slice(1)
+    return newCatArray;
 
 }
